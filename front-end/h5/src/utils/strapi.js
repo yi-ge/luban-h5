596a6a4e6a24eb24c6c05793f999e3c89636bd35
yige <a@wyr.me>
2019-08-22 18:06:54 +0800

    test

--- front-end/h5/src/utils/strapi.js
+++ front-end/h5/src/utils/strapi.js
@@ -1,8 +1,8 @@
-import Strapi from 'strapi-sdk-javascript'
+import axios from 'axios'
 import { API_ORIGIN } from '../constants/api.js'
 
 // TODO 通过 env 来配置后端 URL 等信息
-const strapi = new Strapi({
+const strapi = axios.create({
   baseURL: API_ORIGIN
 })
 
