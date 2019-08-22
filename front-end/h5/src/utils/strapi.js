7671f6cd8a59a3324a42835e1a00ed5f53a062bd
yige <a@wyr.me>
2019-08-22 18:01:34 +0800

    test

--- front-end/h5/src/utils/strapi.js
+++ front-end/h5/src/utils/strapi.js
@@ -1,6 +1,9 @@
 import Strapi from 'strapi-sdk-javascript'
+import { API_ORIGIN } from '../constants/api.js'
 
 // TODO 通过 env 来配置后端 URL 等信息
-const strapi = new Strapi()
+const strapi = new Strapi({
+  baseURL: API_ORIGIN
+})
 
 export default strapi
